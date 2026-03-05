ca7b2fc6529c432a80e1e8942fdf910f6c03d1af
jthomaschung <jchung@atlaswe.com>
2026-03-04 19:38:20 -0800

    Update print statement from 'Hello' to 'Goodbye'

--- jj_wsr_bot.py
+++ jj_wsr_bot.py
@@ -477,7 +477,7 @@ class JimmyJohnsWSRBot:
 
         with sync_playwright() as p:
             browser = p.chromium.launch(
-                headless=False,
+                headless=True,
                 args=['--disable-blink-features=AutomationControlled']
             )
             context = browser.new_context(
@@ -595,3 +595,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
